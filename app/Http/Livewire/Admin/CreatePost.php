e2f6d7fbde6a2cf4d11dc6e719e6de15ccfcb9c1
MosesKet <sirketxp@gmail.com>
2022-08-19 16:08:48 +0100

    Create Post

--- app/Http/Livewire/Admin/CreatePost.php
+++ app/Http/Livewire/Admin/CreatePost.php
@@ -2,30 +2,41 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class CreatePost extends Component
 {
+    public $user;
+    public $post;
     public $title;
     public $body;
     public $message;
-    // public Post $post;
 
     protected $rules = [
         'title' => 'required|string|min:6',
         'body' => 'required|string|max:500',
     ];
 
+    public function mount()
+    {
+        $this->user = Auth::user();
+        $this->post = new Post();
+    }
+
     public function save()
     {
         $this->validate();
+
+        // php artisan make:model Images -m
+
+        $this->nft->user_id = $this->user->id;
+        $this->post->title = $this->title;
+        $this->post->body = $this->body;
         $this->post->save();
 
-    }
-
-    public function mount()
-    {
-        //
+        $this->post->save();
     }
 
     public function render()
