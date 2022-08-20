6c4090751865a2c757e83c08257ca423c3e24002
MosesKet <sirketxp@gmail.com>
2022-08-20 16:31:11 +0100

    cleaning up

--- resources/views/livewire/admin/posts.blade.php
+++ resources/views/livewire/admin/posts.blade.php
@@ -1,23 +1,4 @@
 <div>
-<<<<<<< HEAD
-    <div class="panel-body">
-            <form class="form-horizontal" data-parsley-validate="true" name="demo-form">
-                <div class="form-group row m-b-15">
-                    {{-- <label class="col-md-4 col-sm-4 col-form-label" for="message">comments:</label> --}}
-                    <div class="col-md-8 col-sm-8">
-                        <textarea class="form-control" id="message" name="message" rows="15" placeholder="Comment"></textarea>
-                    </div>
-                </div>
-                <div class="form-group row m-b-0">
-                    {{-- <label class="col-md-4 col-sm-4 col-form-label">&nbsp;</label> --}}
-                    <div class="col-md-8 col-sm-8">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-=======
     <ul class="timeline">
         <li>
             <!-- begin timeline-time -->
@@ -202,7 +183,4 @@
             <!-- begin timeline-body -->
         </li>
     </ul>
-</div>
->>>>>>> dc616a2ac3893a0bd4e3875490d8c6e568c22013
-
 </div>
