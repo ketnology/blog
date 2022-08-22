b6fd5cce38a502e04cfb6052d8a39d828bb4edb1
MosesKet <sirketxp@gmail.com>
2022-08-22 08:27:39 +0100

    post Model adjustments

--- app/Http/Livewire/Admin/Posts.php
+++ app/Http/Livewire/Admin/Posts.php
@@ -7,18 +7,15 @@ use Livewire\Component;
 
 class Posts extends Component
 {
-    // public $post;
+    public $post;
 
-    // public function mount()
-    // {
-    //     $this->post = Post::all();
-    // }
+    public function mount()
+    {
+        $this->post = Post::all();
+    }
 
     public function render()
     {
-        $posts  = Post::all();
-
-        return view('livewire.admin.posts', compact('posts'))->layout('layouts.master');
-        // return view('livewire.admin.posts')->layout('layouts.master');
+        return view('livewire.admin.posts')->layout('layouts.master');
     }
 }
