02a82691732695b18cb9c1a50494344c7119e00b
FESTUS AIRIHENBUWA <fizzypepe@gmail.com>
2022-08-24 17:32:16 +0100

    blog

--- app/Http/Livewire/Blog/Blog.php
+++ app/Http/Livewire/Blog/Blog.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Livewire\Blog;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class Blog extends Component
 {
+    public $posts;
+
+    public function mount()
+    {
+        $this->posts = Post::orderBy('created_at', 'desc')->get();
+    }
     public function render()
     {
         return view('livewire.blog.blog');
